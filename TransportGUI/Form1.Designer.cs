58483aeb297aae2a5060c3804272aa6e1189230c
joegijoegi <jtschopp959@gmail.com>
2020-03-10 14:41:48 +0100

    Search Stations works

--- TransportGUI/Form1.Designer.cs
+++ TransportGUI/Form1.Designer.cs
@@ -35,10 +35,10 @@
             this.btnSearchStartStation = new System.Windows.Forms.Button();
             this.btnSearchEndStation = new System.Windows.Forms.Button();
             this.btnSearchConnection = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgvAbfahrtsplan = new System.Windows.Forms.DataGridView();
             this.btnClose = new System.Windows.Forms.Button();
             this.btnAbfahrtsTafel = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAbfahrtsplan)).BeginInit();
             this.SuspendLayout();
             // 
             // lblFrom
@@ -65,7 +65,7 @@
             this.ddlStartstation.Location = new System.Drawing.Point(47, 6);
             this.ddlStartstation.Name = "ddlStartstation";
             this.ddlStartstation.Size = new System.Drawing.Size(275, 21);
-            this.ddlStartstation.TabIndex = 2;
+            this.ddlStartstation.TabIndex = 1;
             // 
             // ddlEndstation
             // 
@@ -80,39 +80,43 @@
             this.btnSearchStartStation.Location = new System.Drawing.Point(47, 48);
             this.btnSearchStartStation.Name = "btnSearchStartStation";
             this.btnSearchStartStation.Size = new System.Drawing.Size(275, 23);
-            this.btnSearchStartStation.TabIndex = 4;
-            this.btnSearchStartStation.Text = "Passende Verbindung suchen";
+            this.btnSearchStartStation.TabIndex = 2;
+            this.btnSearchStartStation.Text = "Passende Station suchen";
             this.btnSearchStartStation.UseVisualStyleBackColor = true;
+            this.btnSearchStartStation.Click += new System.EventHandler(this.SearchStation_Click);
             // 
             // btnSearchEndStation
             // 
             this.btnSearchEndStation.Location = new System.Drawing.Point(370, 48);
             this.btnSearchEndStation.Name = "btnSearchEndStation";
             this.btnSearchEndStation.Size = new System.Drawing.Size(275, 23);
-            this.btnSearchEndStation.TabIndex = 5;
-            this.btnSearchEndStation.Text = "Passende Verbindung suchen";
+            this.btnSearchEndStation.TabIndex = 4;
+            this.btnSearchEndStation.Text = "Passende Station suchen";
             this.btnSearchEndStation.UseVisualStyleBackColor = true;
+            this.btnSearchEndStation.Click += new System.EventHandler(this.SearchStation_Click);
             // 
             // btnSearchConnection
             // 
             this.btnSearchConnection.Location = new System.Drawing.Point(247, 91);
             this.btnSearchConnection.Name = "btnSearchConnection";
             this.btnSearchConnection.Size = new System.Drawing.Size(183, 23);
-            this.btnSearchConnection.TabIndex = 6;
+            this.btnSearchConnection.TabIndex = 5;
             this.btnSearchConnection.Text = "Verbindung suchen";
             this.btnSearchConnection.UseVisualStyleBackColor = true;
             // 
-            // dataGridView1
+            // dgvAbfahrtsplan
             // 
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.dgvAbfahrtsplan.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 139);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(633, 254);
-            this.dataGridView1.TabIndex = 7;
+            this.dgvAbfahrtsplan.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvAbfahrtsplan.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgvAbfahrtsplan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAbfahrtsplan.Location = new System.Drawing.Point(12, 139);
+            this.dgvAbfahrtsplan.Name = "dgvAbfahrtsplan";
+            this.dgvAbfahrtsplan.ReadOnly = true;
+            this.dgvAbfahrtsplan.Size = new System.Drawing.Size(633, 254);
+            this.dgvAbfahrtsplan.TabIndex = 7;
             // 
             // btnClose
             // 
@@ -120,9 +124,10 @@
             this.btnClose.Location = new System.Drawing.Point(570, 415);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(75, 23);
-            this.btnClose.TabIndex = 8;
+            this.btnClose.TabIndex = 7;
             this.btnClose.Text = "Beenden";
             this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
             // 
             // btnAbfahrtsTafel
             // 
@@ -130,7 +135,7 @@
             this.btnAbfahrtsTafel.Location = new System.Drawing.Point(12, 415);
             this.btnAbfahrtsTafel.Name = "btnAbfahrtsTafel";
             this.btnAbfahrtsTafel.Size = new System.Drawing.Size(75, 23);
-            this.btnAbfahrtsTafel.TabIndex = 9;
+            this.btnAbfahrtsTafel.TabIndex = 6;
             this.btnAbfahrtsTafel.Text = "Abfahrtstafel";
             this.btnAbfahrtsTafel.UseVisualStyleBackColor = true;
             // 
@@ -141,7 +146,7 @@
             this.ClientSize = new System.Drawing.Size(668, 450);
             this.Controls.Add(this.btnAbfahrtsTafel);
             this.Controls.Add(this.btnClose);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgvAbfahrtsplan);
             this.Controls.Add(this.btnSearchConnection);
             this.Controls.Add(this.btnSearchEndStation);
             this.Controls.Add(this.btnSearchStartStation);
@@ -151,7 +156,7 @@
             this.Controls.Add(this.lblFrom);
             this.Name = "Form1";
             this.Text = "Abfahrtsplan";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAbfahrtsplan)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -166,7 +171,7 @@
         private System.Windows.Forms.Button btnSearchStartStation;
         private System.Windows.Forms.Button btnSearchEndStation;
         private System.Windows.Forms.Button btnSearchConnection;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvAbfahrtsplan;
         private System.Windows.Forms.Button btnClose;
         private System.Windows.Forms.Button btnAbfahrtsTafel;
     }
--- TransportGUI/Form1.cs
+++ TransportGUI/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SwissTransport;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,55 @@ namespace TransportGUI
         public Form1()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Event, welches ausgelöst wird, wenn auf einen der Buttons "Passende Station finden" gedrückt wird.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SearchStation_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if(btn == btnSearchStartStation)
+            {
+                SearchStation(ddlStartstation);
+            }
+            else if(btn == btnSearchEndStation)
+            {
+                SearchStation(ddlEndstation);
+            }
+        }
+
+        /// <summary>
+        /// Methode, welche die Stationen suche und diese auflistet in der Combobox.
+        /// </summary>
+        /// <param name="station"></param>
+        private void SearchStation(ComboBox dropDown)
+        {
+            Transport transport = new Transport();
+            Stations st = new Stations();
+
+            if (dropDown.Text.Length <= 2)
+            {
+                MessageBox.Show("Bitte geben Sie mehr Buchstaben ein, um das Ergebnis genauer anzuzeigen");
+            }
+            else
+            {
+                dropDown.Items.Clear();
+                st = transport.GetStations(dropDown.Text);
+                foreach(var element in st.StationList)
+                {
+                    dropDown.Items.Add(element.Name);
+                }
+                dropDown.DroppedDown = true;
+            }
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
